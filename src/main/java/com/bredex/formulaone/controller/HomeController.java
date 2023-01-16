364ae7982a146a81055c790a2fd26b1d04c4c329
Andrási Márton <jm09as@gmail.com>
2023-01-16 20:23:02 +0100

    ver 0.2 and refactoring

--- src/main/java/com/bredex/formulaone/controller/HomeController.java
+++ src/main/java/com/bredex/formulaone/controller/HomeController.java
@@ -3,15 +3,10 @@ package com.bredex.formulaone.controller;
 import com.bredex.formulaone.model.FormulaOneTeam;
 import com.bredex.formulaone.service.FormulaOneService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
